b064e932bda78ba814d2137b83433ac6d113b9ce
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-05-22 16:02:50 -0400

    Now all of the code is inside one big service, which makes cross-referencing work much better.

--- js/newAngularCart.js
+++ js/newAngularCart.js
@@ -32,199 +32,101 @@ function Product(id, name, price, category, img) {
     };
 };
 
-app.factory('productHandler', ['$http', '$log', '$rootScope', function($http, $log, $rootScope) {
+app.factory('storeManager', ['$http', '$log', '$rootScope', 'notifyService', 
+                             function($http, $log, $rootScope, notifyService) {
     
     $rootScope.itemCount = 0;
     
-    var products = {};
-    
-    products.counter = function() {
-        var itemTempCounter = 0;
-        for (var c=0; c < products.list.length; c++) {
-            itemTempCounter += products.quantity.get(c);
-        }
-        
-        $rootScope.itemCount = itemTempCounter;
-    };
-                               
-    products.list = [];
-    
-    products.loadServerDB = function() {
-        if (showDebugOutput) { $log.info("Loading the product database from the server..."); }
-        
-        $http({
-            method: "GET",
-            url: siteURL + "products.json"
-        }).then(function(response) {
-            for (var p = 0; p < response.data.length; p++) {
-                products.list.push(new Product(response.data[p]['itemID'], 
-                                               response.data[p]['itemname'], 
-                                               response.data[p]['price'],
-                                               response.data[p]['category'],
-                                               response.data[p]['img']));
-            }
-            if (showDebugOutput) { $log.info("Product database load from server success."); }
-            products.loadCartFromDisk();
-            
-        });
-    };
-    
-    products.loadCartFromDisk = function() {
-        
-        if (showDebugOutput) { $log.info("Loading the user's cart from disk..."); }
-        
-        var cartFromStorage = localStorage["cart"];
+    var self = {
+        productDB: {
+            list: [],
+            load: function() {
+                if (showDebugOutput) { $log.info("Loading the product database from the server..."); }
 
-        if (cartFromStorage != null && cartFromStorage.length != 0) {
-            cartFromStorage = cartFromStorage.split("|");
-            for (var k=0; k < cartFromStorage.length; k++) {
-                var loadedIndex = products.find(cartFromStorage[k]);
-                if (loadedIndex > -1) {
-                    products.quantity.bump(loadedIndex);
-                }
-            }
-        }
-        
-        if (showDebugOutput) { $log.info("Cart load from disk success."); }
-        
-        products.counter();
-        
-        if (document.getElementById("cart") != null) {
-            products.cartDisplay.setArray();
-        }
-    };
-    
-    products.find = function(id) {
-        id = id.toLowerCase();
-        
-        for (var s=0; s<products.list.length; s++) {
-            if (id == products.list[s].getId().toLowerCase()) {
-                return s;
-            }
-        }
-        $log.error("Product ID '" + id + "' was not found!")
-        return -1;
-    };
-    
-    products.quantity = {
-        bump: function(index) {
-            products.list[index].bumpQuantity();
-//            products.counter();
-        },
-        set: function(index, quantity) {
-            products.list[index].setQuantity(quantity);
-//            products.counter();
-        },
-        clear: function(index) {
-            products.list[index].setQuantity(0);
-//            products.counter();
-        },
-        get: function(index) {
-            return products.list[index].getQuantity();
-        }
-        
-    };
-    
-    products.getItem = function(id) {
-        return products.list[id];
-    };
-    
-    products.cartDisplay = {
-        setArray: function() {
-            
-            if (showDebugOutput) { $log.info("Setting the cart display!"); }
-            
-            if (products.list.length > 0) {
-                var tempCartArray = [];
-                for (var q=0; q < products.list.length; q++) {
-                    if (products.list[q].getQuantity() > 0) {
-                        tempCartArray.push({
-                            name: products.list[q].getName(),
-                            id: products.list[q].getId(),
-                            quantity: products.list[q].getQuantity(),
-                            price: products.list[q].getPrice(),
-                            extPrice: products.list[q].getExtendedPrice(),
-                            img: siteURL + products.list[q].getImgPath()
-                        });
+                $http({
+                    method: "GET",
+                    url: siteURL + "products.json"
+                }).then(function(response) {
+                    for (var p = 0; p < response.data.length; p++) {
+                        self.productDB.list.push(new Product(response.data[p]['itemID'], 
+                                                       response.data[p]['itemname'], 
+                                                       response.data[p]['price'],
+                                                       response.data[p]['category'],
+                                                       response.data[p]['img']));
+                    }
+                    if (showDebugOutput) { $log.info("Product database load from server success."); }
+                    self.cart.loadFromDisk();
+
+                });
+            },
+            find: find = function(id) {
+                id = id.toLowerCase();
+
+                for (var s=0; s < self.productDB.list.length; s++) {
+                    if (id == self.productDB.list[s].getId().toLowerCase()) {
+                        return s;
                     }
                 }
-                
-                $rootScope.cartArray = tempCartArray;
-                $rootScope.showCart = true;
-                
-            } else {
-                $rootScope.showCart = false;
+                $log.error("Product ID '" + id + "' was not found!")
+                return -1;
             }
         },
-        update: function() {
-            
-            if (showDebugOutput) { $log.info("Updating the cart from the display!"); }
-            
-            $('#cart input.productQuantity').each(function() {
-                if ($.isNumeric($(this).val()) && $(this).val() >= 0) {
-                    products.quantity.set(products.find($(this).attr("data-itemid")), $(this).val());
+        cart: {
+            loadFromDisk: function() {
+
+                if (showDebugOutput) { $log.info("Loading the user's cart from disk..."); }
+
+                var cartFromStorage = localStorage["cart"];
+
+                if (cartFromStorage != null && cartFromStorage.length != 0) {
+                    cartFromStorage = cartFromStorage.split("|");
+                    for (var k=0; k < cartFromStorage.length; k++) {
+                        var loadedIndex = self.productDB.find(cartFromStorage[k]);
+                        if (loadedIndex > -1) {
+                            self.cart.quantity.bump(loadedIndex);
+                        }
+                    }
                 }
-            });
-            
-            products.cartDisplay.setArray();
-            products.counter();
-        }
-    }
-    
-    products.loadServerDB();
-    
-    return products;
-}]);
 
-app.service('itemCounterService', ['productHandler', function(productHandler) {
-    return {
-        do: function() {
-            var itemTempCounter = 0
-            for (var c=0; c < productHandler.list.length; c++) {
-                itemTempCounter += productHandler.quantity.get(c);
-            }
-            $log.debug(itemTempCounter);
+                if (showDebugOutput) { $log.info("Cart load from disk success."); }
 
-            return itemTempCounter;
-        }
-    }
-}]);
+                self.cart.itemCounter();
 
-app.provider('cartProvider', function() {
-    // Configuration
-//    this.cartURL = 'cart.html';
-//    this.homeURL = 'index.html';
-//    this.salesTaxRate = 0.06;
-//    this.freeShipping = true;
-//    this.freeShippingMin = 29.99;
-//    this.shippingRate = 8.99;
-////    this.cartName = "cart";
-    
-    this.cartURL = "cart.html";
-    this.homeURL = "index.html";
-    this.salesTaxRate = 0.06;
-    this.freeShipping = false;
-    this.freeShippingMin = 49.00;
-    this.shippingRate = 7.95;
-    this.cartName = "bgparts_cart";
+                if (document.getElementById("cart") != null) {
+                    self.cart.display.setArray();
+                }
+            },
+            quantity: {
+                bump: function(index) {
+                    self.productDB.list[index].bumpQuantity();
+        //            products.counter();
+                },
+                set: function(index, quantity) {
+                    self.productDB.list[index].setQuantity(quantity);
+        //            products.counter();
+                },
+                clear: function(index) {
+                    self.productDB.list[index].setQuantity(0);
+        //            products.counter();
+                },
+                get: function(index) {
+                    return self.productDB.list[index].getQuantity();
+                }
 
-    
-    // Functions available to controllers
-    this.$get = ['notifyService', 'productHandler', '$log', function(notifyService, productHandler, $log) {
-        var cartPVDR = {
+            },
             requestAdd: function(id) {
-                id = productHandler.find(id);
+                id = self.productDB.find(id);
                 
                 if (id > -1) {
-                    if (productHandler.quantity.get(id) == 0) {
-                        notifyService.alert("The item '" + productHandler.list[id].getName() + "' has been added to your cart.");
-                        this.add(id);
+                    if (self.cart.quantity.get(id) == 0) {
+                        notifyService.alert("The item '" + self.productDB.list[id].getName() + "' has been added to your cart.");
+                        self.cart.add(id);
                     } else {
-                        notifyService.confirm("The item '" + productHandler.list[id].getName() + "' is already " +
-                                              "in your cart. Are you sure you want to add another?", "Ad",
+                        notifyService.confirm("The item '" + self.productDB.list[id].getName() + "' is already " +
+                                              "in your cart.", "Add another?",
                                               "Yes", "No").then(function(userInput) {
                             if (userInput) {
-                                cartPVDR.add(id);
+                                self.cart.add(id);
                             }
                         });
                     }
@@ -234,82 +136,144 @@ app.provider('cartProvider', function() {
                 }
             },
             add: function(id) {
-                productHandler.quantity.bump(id);
-                productHandler.counter();
-                this.write();
+                if (showDebugOutput) { $log.info("Adding item with id '" + id + "' to the cart...")}
+                self.cart.quantity.bump(id);
+                self.cart.itemCounter();
+                self.cart.write();
+                if (showDebugOutput) { $log.info("Item with id '" + id + "' was sucessfully added to the cart.")}
             },
             clear: function() {
-                localStorage.removeItem(cartPVDR.cartName);
-                window.location.reload();
+                if (showDebugOutput) { $log.info("Emptying cart..."); }
+                for (var z=0; z < productHandler.list.length; z++) {
+                    self.cart.quantity.set(z, 0);
+                }
+                self.cart.write();
+                self.cart.itemCounter();
+                if (showDebugOutput) { $log.info("Emptying cart success."); }
             },
             toString: function() {
                 var tempCartStrItems = []
                 
-                for (var i=0; i<productHandler.list.length; i++) {
-                    for (var j=0; j<productHandler.quantity.get(i); j++) {
-                        tempCartStrItems.push(productHandler.list[i].getId());
+                for (var i=0; i < self.productDB.list.length; i++) {
+                    for (var j=0; j < self.cart.quantity.get(i); j++) {
+                        tempCartStrItems.push(self.productDB.list[i].getId());
                     }
                 }
                 
                 return tempCartStrItems.join("|");
             },
             write: function() {
+                if (showDebugOutput) { $log.info("Writing cart to disk..."); }
                 localStorage["cart"] = this.toString();
-            }
-        }
-        
-        return cartPVDR;
-    }];
-});
+                if (showDebugOutput) { $log.info ("Writing cart to disk success."); }
+            },
+            itemCounter: function() {
+                var itemTempCounter = 0;
+                for (var c=0; c < self.productDB.list.length; c++) {
+                    itemTempCounter += self.cart.quantity.get(c);
+                }
 
-app.service("cartInterfaceService",  ['$log', '$rootScope', 'productHandler', 
-                                      function($log, $rootScope, productHandler) {
-    serviceObject = {
-        setCartDisplayArray: function() {
-            
-            var tempCartArray = [];
-            
-            for (var q=0; q < productHandler.list.length; q++) {
-                if (productHandler.list[q].getQuantity() > 0) {
-                    tempCartArray.push({
-                        name: productHandler.list[q].getName(),
-                        id: productHandler.list[q].getId(),
-                        quantity: productHandler.list[q].getQuantity(),
-                        price: productHandler.list[q].getPrice(),
-                        extPrice: productHandler.list[q].getExtendedPrice(),
-                        img: siteURL + productHandler.list[q].getImgPath()
+                $rootScope.itemCount = itemTempCounter;
+            },
+            display: {
+                setArray: function() {
+
+                    if (showDebugOutput) { $log.info("Setting cart display..."); }
+
+                    if (self.productDB.list.length > 0) {
+                        var tempCartArray = [];
+                        for (var q=0; q < self.productDB.list.length; q++) {
+                            if (self.productDB.list[q].getQuantity() > 0) {
+                                tempCartArray.push({
+                                    name: self.productDB.list[q].getName(),
+                                    id: self.productDB.list[q].getId(),
+                                    quantity: self.productDB.list[q].getQuantity(),
+                                    price: self.productDB.list[q].getPrice(),
+                                    extPrice: self.productDB.list[q].getExtendedPrice(),
+                                    img: siteURL + self.productDB.list[q].getImgPath()
+                                });
+                            }
+                        }
+
+                        $rootScope.cartArray = tempCartArray;
+                        $rootScope.showCart = true;
+
+                    } else {
+                        $rootScope.showCart = false;
+                    }
+                    
+                    if (showDebugOutput) { $log.info("Setting cart display success."); }
+                    
+                },
+                update: function() {
+
+                    if (showDebugOutput) { $log.info("Updating the cart from the display..."); }
+
+                    $('#cart input.productQuantity').each(function() {
+                        if ($.isNumeric($(this).val()) && $(this).val() >= 0) {
+                            self.cart.quantity.set(self.productDB.find($(this).attr("data-itemid")), $(this).val());
+                        }
                     });
+                    
+                    self.cart.write();
+                    self.cart.display.setArray();
+                    self.cart.itemCounter();
+                    
+                    if (showDebugOutput) { $log.info("Updating cart from display success."); }
+                    
                 }
             }
-            $log.debug(tempCartArray);
-            $rootScope.cartArray = tempCartArray;
-//            $rootScope.$apply();
-        },
-        updateCartFromDisplay: function() {
-            $('#cart input.productQuantity').each(function() {
-                if ($.isNumeric($(this).val()) && $(this).val() >= 0) {
-                    productHandler.quantity.set(productHandler.find($(this).attr("data-itemid")), $(this).val());
-                }
-            });
             
-            serviceObject.setArray();
-            productHandler.counter();
         }
     }
     
-    return serviceObject;
+    self.productDB.load();
+    
+    return self;
 }]);
 
+//app.provider('cartProvider', function() {
+//    // Configuration
+////    this.cartURL = 'cart.html';
+////    this.homeURL = 'index.html';
+////    this.salesTaxRate = 0.06;
+////    this.freeShipping = true;
+////    this.freeShippingMin = 29.99;
+////    this.shippingRate = 8.99;
+//////    this.cartName = "cart";
+//    
+//    this.cartURL = "cart.html";
+//    this.homeURL = "index.html";
+//    this.salesTaxRate = 0.06;
+//    this.freeShipping = false;
+//    this.freeShippingMin = 49.00;
+//    this.shippingRate = 7.95;
+//    this.cartName = "bgparts_cart";
+//
+//    
+//    // Functions available to controllers
+//    this.$get = ['notifyService', 'productHandler', '$log', function(notifyService, productHandler, $log) {
+//        var cartPVDR = {
+//    
+//        }
+//        
+//        return cartPVDR;
+//    }];
+//});
 
-app.controller("cartCtrl", ['$scope', '$log', 'cartProvider', 'productHandler',
-                            function($scope, $log, cartProvider, productHandler) {
+app.controller("cartCtrl", ['$scope', '$log', 'storeManager',
+                            function($scope, $log, storeManager) {
     
     $scope.cartAdd = function(id) {
-        cartProvider.requestAdd(id);
+        storeManager.cart.requestAdd(id);
     }
     
     $scope.updateCart = function() {
-        productHandler.cartDisplay.update();
+        storeManager.cart.display.update();
+    }
+    
+    $scope.emptyCart = function() {
+        storeManager.cart.clear();
     }
     
 }]);
